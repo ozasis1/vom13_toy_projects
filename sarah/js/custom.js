9b63d50e0d1dfb81581590bdca004cb74de2538e
ozasis1 <ozasis1@gmail.com>
2023-04-12 09:24:53 +0900

    list new _ 0422

--- sarah/js/custom.js
+++ sarah/js/custom.js
@@ -2,27 +2,26 @@
 console.log(imgWrapper);
 let images = [];
 for (let i = 0; i < 200; i++) {
-	images += `<img src='img/pic${i}.jpg'>`;
+  images += `<img src='img/pic${i}.jpg'>`;
 }
 
 // console.log(images);
 
 imgWrapper.innerHTML = images;
 
-imgWrapper.addEventListener('mousemove', function(e){
-	// console.log(e);
-	let x = e.offsetX;
-	const screenW = document.body.offsetWidth;
-	const imgTga = document.querySelectorAll('section img');
-	// console.log(screenW);
-	// console.log(x);
-	imgTags.forEach((tag)=> {
-		tag.style.display = 'none';
-	});
+imgWrapper.addEventListener('mousemove', function (e) {
+  let x = e.offsetX;
+  const screenW = document.body.offsetWidth;
+  const imgTags = document.querySelectorAll('section img');
 
-	const percent = Math.floor((x / screenW)*200);
-	// console.log(percent);
-	imgTags[percent].style.display = 'block';
+  imgTags.forEach((tag) => {
+    tag.style.display = 'none';
+  });
+
+  const percent = Math.floor((x / screenW) * 200);
+  // console.log(percent);
+
+  imgTags[percent].style.display = 'block';
 });
 
 // 커서 : 10px
@@ -30,7 +29,7 @@ imgWrapper.addEventListener('mousemove', function(e){
 // 브라우저 : 1000px
 
 // (커서 / 브라우저) * 200
-// 0.01*200
+// 0.01 * 200
 // 2
-// 0.988*200
+// 0.988 * 200
 // 197.6
